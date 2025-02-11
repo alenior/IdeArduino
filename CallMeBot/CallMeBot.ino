202790ee499e01b4e434dc5b5adc3b78a1ae1a46
alenior <alenior@gmail.com>
2025-02-11 18:29:58 -0300

    CallMeBot

---
+++ CallMeBot/CallMeBot.ino
@@ -0,0 +1,57 @@
+/* 
+ https://RandomNerdTutorials.com/esp32-send-messages-whatsapp/
+*/
+
+#include <WiFi.h>    
+#include <HTTPClient.h>
+#include <UrlEncode.h>
+
+const char* ssid = "SanUSB";
+const char* password = "**********";
+
+// +international_country_code + phone number
+// Portugal +351, example: +351912345678
+String phoneNumber = "+558582*****2"; 
+String apiKey = "19****9"; 
+
+void sendMessage(String message){
+
+  // Data to send with HTTP POST
+  String url = "https://api.callmebot.com/whatsapp.php?phone=" + phoneNumber + "&apikey=" + apiKey + "&text=" + urlEncode(message);    
+  HTTPClient http;
+  http.begin(url);
+
+  // Specify content-type header
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  
+  // Send HTTP POST request
+  int httpResponseCode = http.POST(url);
+  if (httpResponseCode == 200){
+    Serial.print("Message sent successfully");
+  }
+  else{
+    Serial.println("Error sending the message");
+    Serial.print("HTTP response code: ");
+    Serial.println(httpResponseCode);
+  }
+
+  // Free resources
+  http.end();
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  // Send Message to WhatsAPP
+  sendMessage("Hello from SanUSB ESP32 through callmebot!");
+}
